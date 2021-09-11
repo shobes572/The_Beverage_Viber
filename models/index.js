1b90484096a1607d2e78d5d073fd5c151fd33584
John-Mark Sackett <jmsackett91@gmail.com>
2021-09-11 12:29:10 -0400

    added a favorite model and linked it to the bev and user

--- models/index.js
+++ models/index.js
@@ -1,12 +1,30 @@
 const User = require('./user');
 const Review = require('./review');
 const Beverage = require('./beverage');
+const Favorite = require('./favorite');
 
 User.hasMany(Review, {
     foreignKey: 'user_id'
 });
 Review.belongsTo(User, {
     foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Beverage.belongsToMany(User, {
+    through: {
+        model: Favorite,
+        unique: false
+    },
+    as: 'user_beverages',
+    onDelete: "cascade"
+});
+User.belongsToMany(Beverage, {
+    through: {
+        model: Favorite,
+        unique: false
+    },
+    as: 'beverage_favorites',
     onDelete: "cascade"
 });
 
@@ -18,4 +36,4 @@ Review.belongsTo(Beverage, {
     onDelete: "cascade"
 });
 
-module.exports = { User, Review, Beverage };
+module.exports = { User, Review, Beverage, Favorite };
