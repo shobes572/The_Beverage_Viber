17fe3039d840eac18890f55b1612babf7c8a2508
John-Mark Sackett <jmsackett91@gmail.com>
2021-09-14 19:25:59 -0400

    added a beverage routes file with a get single request and added it into the index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const beverageRoutes = require('./beverage-routes');
 
 router.use('/users', userRoutes);
+router.use('/beverage', beverageRoutes);
 
 module.exports = router;
