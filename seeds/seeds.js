ca1bdd75a561b68045686c11baafbc58cb22236d
Shobair Abidi <shobair.abidi@gmail.com>
2021-09-14 20:20:10 -0400

    ENV vars updated in connection.js, updated beverage model, added beverage scraping to db

---
+++ seeds/seeds.js
@@ -0,0 +1,11 @@
+const sequelize = require('../config/connection');
+const { User, Review, Beverage, Favorite } = require('../models');
+const seedDataBeverages = require('./beverage_db.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    const beverages = await Beverage.bulkCreate(seedDataBeverages);
+    process.exit(0);
+}
+
+seedDatabase();
