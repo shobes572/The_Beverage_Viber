89fc1e6daf6ca46e1de8b135a3c9edb97771836e
catupperman <catupperman@gmail.com>
2021-09-16 18:56:36 -0400

    home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
-const { User, Review, Favorite, Beverage } = require('../models');
-const { post } = require('./api');
+const { User, Beverage } = require('../models');
+
+//Content shows only if the user is logged in
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+      });
+  
+      const users = userData.map((project) => project.get({ plain: true }));
+  
+      res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+      });
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
 
 router.get('/login-signup', async (req, res) => {
     if (req.session.logged_in){
@@ -9,6 +30,8 @@ router.get('/login-signup', async (req, res) => {
         res.render('homepage');
     }
 });
+
+module.exports = router;
 
 
 
